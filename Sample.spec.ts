9bc3819a57b8301200fd66dff867504e2283c8b6
asrivastava82 <78613350+asrivastava82@users.noreply.github.com>
2025-07-29 17:56:00 +0530

    latest1 change

--- Sample.spec.ts
+++ Sample.spec.ts
@@ -4,12 +4,9 @@ test('sample test', async ({ }) => {
 
   // Your test code here
  // This is new code
-  //new linw
+  //this is good
 
-<<<<<<< HEAD
-=======
  
->>>>>>> f703a7912e4c9aa8c17d8a05107fcb49353542c0
 });
 
 
